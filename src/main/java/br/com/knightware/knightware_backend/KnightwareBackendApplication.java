dce44f52665bce7ca788c4ce87c94feae281f2ba
Gabriel Alexandre <galexandre2002@gmail.com>
2025-08-08 00:05:30 -0300

    - commit inicial do projeto

---
+++ src/main/java/br/com/knightware/knightware_backend/KnightwareBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.knightware.knightware_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnightwareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnightwareBackendApplication.class, args);
+	}
+
+}
